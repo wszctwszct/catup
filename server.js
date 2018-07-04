f0f318f53f9f6fe7169e031ead0a01bb865a274e
Christopher Chao <jctnee@hotmail.com>
2018-07-04 09:13:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const database = 'nodemysql';
 
+
 //create connection
 /*const db = mysql.createConnection({
     host            : 'xxx',
